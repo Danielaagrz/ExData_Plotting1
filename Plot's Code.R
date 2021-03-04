d7c15ed16d03f48a5be2b0a986f96a02ed7a18ec
Danielaagrz <daniela.agrz12@gmail.com>
2021-03-04 15:12:07 -0700

    Project One

---
+++ Plot's Code.R
@@ -0,0 +1,53 @@
+
+#Download the Data
+EPower<- read.table("file:///Users/danielaaguilar/Documents/repo/datasciencecoursera/household_power_consumption.txt", sep = ";", header=TRUE, na.strings = "?")
+
+#Convert Date and Time variable to their correct class
+EPower$Date<-as.Date(EPower$Date, format = "%d/%m/%Y")
+
+#Subset the dates: 2007-02-01 and 2007-02-02
+EPower2<-subset(EPower, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+#First Plot
+quartz()
+
+hist(EPower2$Global_active_power, 
+     breaks= 12, main="Global Active Power",
+     xlab= "Global Active Power (kilowatts)",
+     ylab="Frequency", col="red", ylim= c(0,1200))
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
+
+
+#Combine the Date and Time columns 
+EPower2<- EPower2 %>% mutate(DateTime=paste(Date, Time))
+
+#Convert to format as.POSIXct
+EPower2$DateTime<- as.POSIXct(EPower2$DateTime)
+
+#Second Plot 
+plot(EPower2$DateTime, EPower2$Global_active_power, type = "l", xlab = "", ylab=" Global Active Power")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
+
+#Third Plot
+with(EPower2,{plot(DateTime,Sub_metering_1, type="l", xlab = "", ylab = "Energy Sub Metering")
+  lines(DateTime, Sub_metering_2, type="l", col="red")
+  lines(DateTime, Sub_metering_3, type = "l", col="blue")
+  legend("topright", c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
+})
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
+
+#Fourth Plot
+par(mfrow= c(2,2))
+plot(EPower2$DateTime, EPower2$Global_active_power, type = "l", xlab = "", ylab=" Global Active Power")
+plot(EPower2$DateTime, EPower2$Voltage, type = "l", xlab = "datetime", ylab="Voltage")
+with(EPower2,{plot(DateTime,Sub_metering_1, type="l", xlab = "", ylab = "Energy Sub Metering")
+  lines(DateTime, Sub_metering_2, type="l", col="red")
+  lines(DateTime, Sub_metering_3, type = "l", col="blue")
+  legend("topright", c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
+})
+plot(EPower2$DateTime, EPower2$Global_reactive_power, type = "l", xlab = "datetime", ylab="Global_reactive_power")
+dev.copy(png,"plot4.png", width=480, height=480)
+dev.off()
